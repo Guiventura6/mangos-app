4d12bee4e955293ad97dfd20d3bdc25e7c364997
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-10-10 10:36:17 -0300

    Incluindo menu lateral na main activity

--- app/src/main/java/com/example/mangosapp/MainActivity.java
+++ app/src/main/java/com/example/mangosapp/MainActivity.java
@@ -1,14 +1,114 @@
 package com.example.mangosapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Mangos");
+        setSupportActionBar(toolbar);
+
+        bottomNavigationView=findViewById(R.id.bottomNavigationbar);
+        frameLayout=findViewById(R.id.main_frame);
+        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(
+                this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close
+        );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView=findViewById(R.id.naView);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void onBackPressed() {
+
+        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        }else {
+            super.onBackPressed();
+        }
+
+
+    }
+
+    public void displaySelectedListener(int itemId){
+
+        Fragment fragment=null;
+
+        switch (itemId){
+            case R.id.principal:
+                // fragment=new DashBoardFragment();
+                break;
+
+            case R.id.chart:
+                // fragment=new IncomeFragment();
+                break;
+
+            case R.id.target:
+                // fragment=new ExpenseFragment();
+                break;
+
+            case R.id.school:
+                // fragment=new TargetFragment();
+                break;
+
+            case R.id.logout:
+                // mAuth.signOut();
+                //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                break;
+
+        }
+
+        if (fragment!=null){
+            FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.main_frame, fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+
 }
