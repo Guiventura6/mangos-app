7e3e6e372b2dae7365f3504ad33310fdf31d1318
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-10-11 19:06:44 -0300

    Inclundo botoes na tela home e animação

--- app/src/main/java/com/example/mangosapp/HomeFragment.java
+++ app/src/main/java/com/example/mangosapp/HomeFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +19,21 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    //Floating button
+    private FloatingActionButton fab_main_btn;
+    private FloatingActionButton fab_ganhos_btn;
+    private FloatingActionButton fab_gastos_btn;
+
+    // Floating button textview..
+    private TextView fab_ganhos_txt;
+    private TextView fab_gastos_txt;
+
+    //boolen
+    private boolean isOpen=false;
+
+    //Animation.
+    private Animation FadeOpen, FadeClose;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +79,79 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View myview = inflater.inflate(R.layout.fragment_home, container, false);
+
+        FadeOpen= AnimationUtils.loadAnimation(getActivity(), R.anim.fade_open);
+        FadeClose=AnimationUtils.loadAnimation(getActivity(), R.anim.fade_close);
+
+        //Connect floating button to layout
+        fab_main_btn=myview.findViewById(R.id.fb_main_plus_btn);
+        fab_ganhos_btn=myview.findViewById(R.id.ganhos_ft_btn);
+        fab_gastos_btn=myview.findViewById(R.id.gastos_ft_btn);
+
+        // Connect floating text
+        fab_ganhos_txt=myview.findViewById(R.id.income_ft_text);
+        fab_gastos_txt=myview.findViewById(R.id.expense_ft_text);
+
+        fab_main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isOpen){
+                    fab_ganhos_btn.startAnimation(FadeClose);
+                    fab_gastos_btn.startAnimation(FadeClose);
+                    fab_ganhos_btn.setClickable(false);
+                    fab_gastos_btn.setClickable(false);
+
+                    fab_ganhos_txt.startAnimation(FadeClose);
+                    fab_gastos_txt.startAnimation(FadeClose);
+                    fab_ganhos_txt.setClickable(false);
+                    fab_gastos_txt.setClickable(false);
+                    isOpen=false;
+                }else {
+                    fab_ganhos_btn.startAnimation(FadeOpen);
+                    fab_gastos_btn.startAnimation(FadeOpen);
+                    fab_ganhos_btn.setClickable(true);
+                    fab_gastos_btn.setClickable(true);
+
+                    fab_ganhos_txt.startAnimation(FadeOpen);
+                    fab_gastos_txt.startAnimation(FadeOpen);
+                    fab_ganhos_txt.setClickable(true);
+                    fab_ganhos_txt.setClickable(true);
+                    isOpen=true;
+                }
+            }
+        });
+
+
         return myview;
     }
+
+    //Floating button animation
+    private void ftAnimation(){
+        if (isOpen){
+
+            fab_ganhos_btn.startAnimation(FadeClose);
+            fab_gastos_btn.startAnimation(FadeClose);
+            fab_ganhos_btn.setClickable(false);
+            fab_gastos_btn.setClickable(false);
+
+            fab_ganhos_txt.startAnimation(FadeClose);
+            fab_gastos_txt.startAnimation(FadeClose);
+            fab_ganhos_txt.setClickable(false);
+            fab_gastos_txt.setClickable(false);
+            isOpen=false;
+        }else {
+            fab_ganhos_btn.startAnimation(FadeOpen);
+            fab_gastos_btn.startAnimation(FadeOpen);
+            fab_ganhos_btn.setClickable(true);
+            fab_gastos_btn.setClickable(true);
+
+            fab_ganhos_txt.startAnimation(FadeOpen);
+            fab_gastos_txt.startAnimation(FadeOpen);
+            fab_ganhos_txt.setClickable(true);
+            fab_ganhos_txt.setClickable(true);
+            isOpen=true;
+        }
+    }
+
 }
