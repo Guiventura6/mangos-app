2821aaf3170738ae6e8dab35e71f3d7cbbe43fdf
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-10-18 09:34:53 -0300

    Criação do layout do objetivo e alteração na classe goals

--- app/src/main/java/com/example/mangosapp/Model/Goals.java
+++ app/src/main/java/com/example/mangosapp/Model/Goals.java
@@ -3,19 +3,23 @@ package com.example.mangosapp.Model;
 public class Goals {
 
     private String id;
-    private String description;
+    private String title;
+    private String reason;
     private String created;
     private String deadline;
     private int amount;
+    private int total_amount;
 
     public Goals() {}
 
-    public Goals(String id, String description, String created, String deadline, int amount) {
+    public Goals(String id, String title, String reason, String created, String deadline, int amount, int total_amount) {
         this.id = id;
-        this.description = description;
+        this.title = title;
+        this.reason = reason;
         this.created = created;
         this.deadline = deadline;
         this.amount = amount;
+        this.total_amount = total_amount;
     }
 
     public String getId() {
@@ -26,12 +30,20 @@ public class Goals {
         this.id = id;
     }
 
-    public String getDescription() {
-        return description;
+    public String getTitle() {
+        return title;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
     }
 
     public String getCreated() {
@@ -56,5 +68,13 @@ public class Goals {
 
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    public int getTotal_amount() {
+        return total_amount;
+    }
+
+    public void setTotal_amount(int total_amount) {
+        this.total_amount = total_amount;
     }
 }
