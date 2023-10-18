6deb2b3cad8601579898e4facc784739a818007f
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-10-18 15:15:37 -0300

    Adicionando grafico exemplo na tela insigts e add dependencias

--- app/src/main/java/com/example/mangosapp/InsightFragment.java
+++ app/src/main/java/com/example/mangosapp/InsightFragment.java
@@ -1,5 +1,6 @@
 package com.example.mangosapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,56 +9,60 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link InsightFragment# newInstance} factory method to
  * create an instance of this fragment.
  */
 public class InsightFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    // private static final String ARG_PARAM1 = "param1";
-    // private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    // private String mParam1;
-    // private String mParam2;
-
-    // public InsightFragment() {
-        // Required empty public constructor
-    // }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param // param1 Parameter 1.
-     * @param // param2 Parameter 2.
-     * @return A new instance of fragment InsightFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    /* public static InsightFragment newInstance(String param1, String param2) {
-        InsightFragment fragment = new InsightFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }*/
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }*/
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View myview = inflater.inflate(R.layout.fragment_insight, container, false);
+
+        // Obtenha uma referência ao gráfico de pizza no layout
+        PieChart pieChart = myview.findViewById(R.id.pieChart);
+
+        // Crie uma lista de entradas para o gráfico de pizza (categorias e seus gastos)
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(30f, "Alimentação"));
+        entries.add(new PieEntry(20f, "Transporte"));
+        entries.add(new PieEntry(50f, "Outros"));
+
+        // Crie um conjunto de dados com as entradas
+        PieDataSet dataSet = new PieDataSet(entries, "Gastos por Categoria");
+
+        // Configuração das cores
+        dataSet.setColors(Color.rgb(255, 0, 0), Color.rgb(0, 255, 0), Color.rgb(0, 0, 255));
+
+        // Crie o objeto de dados para o gráfico
+        PieData pieData = new PieData(dataSet);
+
+        // Configure o formato dos valores
+        pieData.setValueTextSize(12f);
+        pieData.setValueTextColor(Color.WHITE);
+
+        // Configure o objeto de dados no gráfico
+        pieChart.setData(pieData);
+
+        // Adicione uma descrição para o gráfico (opcional)
+        pieChart.getDescription().setEnabled(false);
+
+        // Atualize o gráfico
+        pieChart.invalidate();
+
         return myview;
     }
 }
